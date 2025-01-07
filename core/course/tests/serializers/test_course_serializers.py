023f8287661a8187d6d7f978702e1d22e355cc55
pranaydeep139 <pranaydeep139@gmail.com>
2025-01-07 19:35:18 +0530

    Test cases - The Beginning

---
+++ core/course/tests/serializers/test_course_serializers.py
@@ -0,0 +1,20 @@
+# tests/serializers/test_course_serializers.py
+from django.test import TestCase
+from ....course.serializers import CourseDetailSerializer
+from ..factories import CourseFactory
+
+class TestCourseSerializer(TestCase):
+    def setUp(self):
+        self.course = CourseFactory()
+        self.serializer = CourseDetailSerializer(instance=self.course)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        expected_fields = {
+            'course_id', 'name', 'description',
+            'visibility', 'module_count'
+        }
+        assert set(data.keys()) >= expected_fields
+
+    def test_module_count_value(self):
+        assert isinstance(self.serializer.data['module_count'], int)
--- core/settings.py
+++ core/settings.py
@@ -241,6 +241,9 @@ SPECTACULAR_SETTINGS = {
         {"name": "Section Items", "description": "Endpoints for Section Items operations"},
         {"name": "Item", "description": "Endpoints for Section Item operations"},
         {"name": "Institution", "description": "Endpoints for Institution operations"},
+        {"name": "Users", "description": "Endpoints for User management"},
+        {"name": "User Institutions", "description": "Endpoints for User-Institution binding management"},
+        {"name": "User Courses", "description": "Endpoints for User-Course binding management"},
     ],
 }
 
